cd4fbd3a6cc97ed239e261e90af0f3720703f4bc
sistemasiq <hflores@sistemasiq.com>
2023-03-28 07:56:35 -0600

    se agrego nuevo controlador para recuperar un plan

--- src/main/java/mx/tecnm/piedad/controladores/PlanREST.java
+++ src/main/java/mx/tecnm/piedad/controladores/PlanREST.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,6 +29,12 @@ public class PlanREST {
 	@GetMapping("/cicd")
 	public ResponseEntity<?> holaCICD(){
 		return new ResponseEntity<String>("Hola CI/CD", HttpStatus.OK);
+	}
+	
+	@GetMapping("/id")
+	public ResponseEntity<?> recuperarPorId(@PathVariable int id){
+		Plan resultado = null;
+		return new ResponseEntity<Plan>(resultado, HttpStatus.OK);
 	}
 
 }
